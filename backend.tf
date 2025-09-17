0ecfaddae12747e7e74284a64c676e65e4808b95
Sidharth Vijayakumar <71686422+sidharthvijayakumar@users.noreply.github.com>
2025-09-17 15:47:04 +0530

    Feat/role update (#13)
    * updated the role name and formated the code
    
    * updated the role arn
    
    * updated s3 to handle state locking
    
    * updated the role name for destroy workflow
    
    * updated the tofu destroy to use secret
    
    * updated the fmt for backend
    
    * updated the workflow to use develop env
    
    * updated the environemnt for all workflows
    
    * updated to run all the time

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 #Use this in case you want to have s3 as backend
 terraform {
   backend "s3" {
-    bucket  = "open-tofu-backend"
-    key     = "tofu-state-file.tfstate"
-    region  = "ap-south-1" # Or your desired region
-    encrypt = true
-    use_lockfile   = true # Enable S3 native locking
+    bucket       = "open-tofu-backend"
+    key          = "tofu-state-file.tfstate"
+    region       = "ap-south-1" # Or your desired region
+    encrypt      = true
+    use_lockfile = true # Enable S3 native locking
   }
 }
